f42716b701018c09f7d807f456b1aeb263f2fbdd
Darshan <darshanjain.godha@gmail.com>
2023-03-08 11:00:46 +0530

    my project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Link , useNavigate} from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
+
+
+const Navbar = () => {
+
+const {user , logOut} = UserAuth()
+const navigate = useNavigate()
+
+const handleLogout = async () => {
+try{
+  await logOut();
+  navigate('/')
+}catch(error){
+  console.log(error)
+}
+
+}
+
+
+  return (
+ <div className='flex items-center justify-between p-4 z-[100] w-full absolute ' >
+<Link to="/">
+<h1  className='text-blue-400  text-4xl font-bold cursor-pointer'  >DJzone</h1>
+
+</Link>
+{user?.email ? (<div>
+   <Link to="/account">
+   <button  className='bg-pink-600 px-6 py-2 rounded mx-4 cursor-pointer text-white' >Account</button>
+   </Link>
+    
+    <button
+    onClick={handleLogout}
+    className='bg-yellow-500 px-6 py-2 rounded cursor-pointer text-white'  >Logout</button>
+    
+</div> ):(<div>
+   <Link to="/login">
+   <button  className='bg-pink-600 px-6 py-2 rounded mx-4 cursor-pointer text-white' >Sign In</button>
+   </Link>
+    <Link to="/signup">
+    <button className='bg-yellow-500 px-6 py-2 rounded cursor-pointer text-white'  >Sign Up</button>
+    </Link>
+</div>)}
+
+ </div>
+  )
+}
+// text-white pr-4 bg-green-600
+export default Navbar
